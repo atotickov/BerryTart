3eeffa2d70d2f835916a257fba82700f3c458b49
atotickov <a.totickov1@gmail.com>
2023-11-28 14:52:06 +0300

    .

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python3
+__author__ = 'atotickov'
 
 from pathlib import Path
 from os.path import join, dirname
 from setuptools import setup, find_packages
 
-dependencies = ['argparse', 'collections', 'gzip']
+dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'biopython',
+                'lxml', 'beautifulsoup4', 'requests', 'gzip']
 
 setup(name='BerryTart',
+      version='0.1',
       packages=find_packages(),
+      author='Azamat Totikov',
+      author_email='a.totickov1@gmail.com',
       install_requires=dependencies,
       long_description=open(join(dirname(__file__), 'README.md')).read(),
       scripts=list(map(str, sorted(Path('python_scr/').rglob("*.py")))))
